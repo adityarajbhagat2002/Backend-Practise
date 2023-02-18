7e5dfb983437efd1606f5763a144eece7a0693e9
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2023-02-19 03:06:04 +0530

    new

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-const http = require('http')
-
-const server= http.createServer((req,res)=>{
-    if(req.url=== '/' ){
-       return res.end('Welcome to our home page !!!')
-    }
-    if(req.url === '/about'){
-       return res.end('here lies the short life we lived !!!!')
-    }
-    res.end(`
-    <h1>Ooops!!</h1>
-    <p>we can not find the address you are trying to acess</p>
-    <a href="/">back home</a>
-    `)
-})
-
-server.listen(5000)
+console.log("Hello world !!!")
